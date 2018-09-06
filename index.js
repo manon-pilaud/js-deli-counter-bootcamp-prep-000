22c876b7e7ec4a9a88da76fb37212fe4222eaf1d
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-06 03:01:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(line){
   }
 
   for (var i=0; i<line.length ;i+=1){
-    listLine.push(i+1+'.'+ line[i])
+    listLine.push( i+1+ '.'+ line[i])
   }
 return ('The line is currently:'+ (listLine.join()))
 }
