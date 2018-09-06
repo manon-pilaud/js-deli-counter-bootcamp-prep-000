6dc216be1909d8a7ad2dc19f2c0d3bef69aeb9a1
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-06 02:09:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDelLine = [];
 function takeANumber (num, name) {
   katzDelLine.push(name)
-  var line = parseInt(num) + 1
+  var line = (parseInt(num) + 1)
   return (`Welcome, ${name}. You are number ${line} in line.`)
 }
