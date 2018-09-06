f2917e933a7dcb2800f70bdc7e85678823a42442
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-06 02:13:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ var katzDeliLine = [];
 function takeANumber(line, name){
   line.push(name);
   
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-  
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
