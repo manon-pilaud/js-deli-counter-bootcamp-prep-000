7a3277255ba23273a8c0de6617868b6ebbb9a929
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-06 01:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDelLine = [];
 function takeANumber (currentLine, name) {
-  currentLine = parseInt(currentLine) +1
+  
   return (`Welcome, ${name}. You are number ${currentLine} in line.`)
 }
